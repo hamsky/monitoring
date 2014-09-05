c0b6553b5f491a386722d2dd4896bb22d3f9fa14
hamsky <hamsky@hamsky>
2014-09-05 11:45:20 +0400

    wrk

--- app/view/tabs/SMEVU.js
+++ app/view/tabs/SMEVU.js
@@ -1,6 +1,20 @@
-Ext.define('monitoring.view.SMEVU',{
-    extend:'Ext.grid.Panel'
-    
-    
+Ext.define('monitoring.view.SMEVU', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.smevu',
+    store: 'SMEVUStore',
+    itemId: 'SMEVUGrid',
+    plugins: [
+        Ext.create('Ext.grid.plugin.RowEditing', {
+            clicksToEdit: 2,
+            clicksToMoveEditor: 1,
+            autoCancel: false
+        })],
+    initComponent: function() {
+     this.columns=[]
+    },
+    tbar: [
+    ],
+    bbar: {
+    }
 });
 
