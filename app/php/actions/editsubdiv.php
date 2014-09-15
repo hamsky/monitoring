ab606fb6a3815a4496ba45b58832706e26f2dc54
hamsky <hamsky@hamsky>
2014-09-15 15:59:26 +0400

    wrk

--- app/php/actions/editsubdiv.php
+++ app/php/actions/editsubdiv.php
@@ -1,5 +1,17 @@
 <?php
+session_start();
+require '../../../inc/dbc.php';
+$id = $_POST['id'];
+$subdiv = $_POST['subdiv'];
+$org = $_POST['org'];
 
+$query = "UPDATE orgsubdiv SET name ='$subdiv', orgId=$org WHERE id = $id";
+
+if (mysql_query($query)) {
+    echo "{success:true}";
+} else {
+    echo "{success:false, error: " . mysql_error() . "}";
+}
 
 ?>
 
