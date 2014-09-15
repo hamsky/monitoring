4c3a959f41fe642f855fd3d69977838a25b6226d
hamsky <hamsky@hamsky>
2014-09-15 15:27:46 +0400

    wrk

---
+++ app/php/actions/deletesubdivsrv.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+require '../../../inc/dbc.php';
+
+$info = $_POST['services'];
+$data = json_decode(stripslashes($info));
+$id = $data[0]->id;
+$query = "DELETE FROM iogv_service WHERE id=$id";
+if (mysql_query($query)) {
+    echo "{success:true}";
+} else {
+    echo "{success:false,err:" . mysql_error() . "}";
+}
+?>
+
