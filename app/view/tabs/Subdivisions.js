f3fa09f21348afe540185269ca5a482a217ae231
hamsky <hamsky@hamsky>
2014-09-15 16:55:04 +0400

    wrk

--- app/view/tabs/Subdivisions.js
+++ app/view/tabs/Subdivisions.js
@@ -10,8 +10,24 @@ Ext.define('monitoring.view.tabs.Subdivisions', {
         Ext.create('Ext.grid.plugin.RowEditing', {
             clicksToEdit: 2,
             clicksToMoveEditor: 1,
-            autoCancel: false
-        
+            autoCancel: false,
+            listeners: {
+                canceledit: function(editor, e, eOpts) {
+                    console.log('cancel edit');
+                },
+                edit: function(editor, e, eOpts) {
+                    Ext.Ajax.request({
+                        url: 'monitoring/actions/subdivedit.php',
+                        params: e.record.getData(),
+                        success: function(response, options) {
+                            // e.record.commit();
+                            Ext.ComponentQuery.query('#subdivGrid')[0].getStore().reload();
+                        }
+
+                    });
+                }
+            }
+
         })
     ],
     initComponent: function() {
