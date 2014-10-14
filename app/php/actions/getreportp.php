877cf25104483ddf4c83dab82dc3f5bcabafe562
hamsky <hamsky@hamsky>
2014-10-14 09:22:54 +0400

    wrk

--- app/php/actions/getreportp.php
+++ app/php/actions/getreportp.php
@@ -4,33 +4,24 @@ require '../../../inc/dbc.php';
 
 $iogv = $_POST['org'];
 $date = $_POST['date'];
+$subdiv = $_POST['subdiv'];
+$query = "select iogv_service.iogv,iogv_service.service, services.service as srv, orgsubdiv.name as subdiv, reports.date as rdate,reports.value as rval,reports.complaints as complaints, reports.gcompl as gcompl from iogv_service, services, orgsubdiv, reports where iogv_service.iogv = $iogv and iogv_service.subdivId =$subdiv and services.id= iogv_service.service and orgsubdiv.id = iogv_service.subdivId and reports.org=iogv_service.iogv and reports.service = iogv_service.service and DATE_FORMAT(reports.date, '%m/%Y')='$date'";
 
-$query = "select reports.id, reports.date, reports.service as srvid, reports.value, reports.complaints, reports.gcompl, services.service from reports, services where org=$iogv and services.id=reports.service and DATE_FORMAT(reports.date, '%m/%Y')='$date'";
+
 $result = mysql_query($query);
-
 while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-
-    $service = $row['srvid'];
-
-    $query2 = "select orgsubdiv.name from orgsubdiv, iogv_service where (iogv_service.iogv=$iogv and iogv_service.service=$service) and orgsubdiv.id = iogv_service.subdivId limit 1";
-
-    $result2 = mysql_query($query2);
-
-    while ($row2 = mysql_fetch_array($result2, MYSQL_ASSOC)) {
-        $subdiv = $row2['name'];
-    }
     $data[] = array(
-        'value' => $row['value'],
-        'service' => $row['service'],
-        'date' => $row['date'],
+        'value' => $row['rvalue'],
+        'service' => $row['srv'],
+        'date' => $row['rdate'],
         'complaints' => $row['complaints'],
         'gcompl' => $row['gcompl'],
-        'subdiv' => $subdiv
+        'subdiv' => $row['subdiv']
     );
 }
 
 $res = array_slice($data, $_GET['limit'] * ($_GET['page'] - 1), $_GET['limit']);
-$myData = array( 'total' => count($data), 'services' => $res);
+$myData = array('total' => count($data), 'services' => $res);
 $d = json_encode($myData);
 echo $d;
 ?>
